b77b16bbe42883bd3d07125877f7c54a6562d570
a.carocavillegas <SU-CORREO-ELECTRONICO>
2021-12-01 17:09:56 -0300

    Primer puntero

---
+++ uno.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main(){
+
+    char *apuntador;
+
+    char letra = 'a';
+
+    apuntador = &letra;
+
+    *apuntador = 'x';
+
+    //cout<<apuntador;
+    //cout<<*apuntador;
+    cout<<letra;
+    
+
+}
